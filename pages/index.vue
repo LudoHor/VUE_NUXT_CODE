9d5bb75924e2e0c529ea6aaabb0061ac18807b3a
LudoHor <ludovit.horvath.94@gmail.com>
2021-09-30 13:37:13 +0200

    remove

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <h1 class="text-2xl text-gray-600 text-center m-2">
         Vitajte na stranke complex Concept
       </h1>
-      <nuxt-img src="/pic/8401.jpg" alt="" />
+      <!-- <nuxt-img src="/pic/8401.jpg" alt="" /> -->
     </div>
   </section>
 </template>
