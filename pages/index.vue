c641853bb8ada90569ed135b34e6f99c7c2bf002
LudoHor <ludovit.horvath.94@gmail.com>
2021-10-29 10:48:10 +0200

    nuxt-img

--- pages/index.vue
+++ pages/index.vue
@@ -3,19 +3,13 @@
     <div class="grid justify-items-center">
       <h1 class="text-3xl text-gold-100 text-center m-2">Vitajte na stránke</h1>
       <div class="section max-w-xl">
-        <transition
+        <!-- <transition
           enter-class="transform scale-50"
           enter-to-class="opacity-100 "
           enter-active-class="transition duration-1000 ease-in-out"
-        >
-          <img
-            class="object-center"
-            src="/pic/CC.jpg"
-            @load="loadLogo"
-            v-show="isLoadedLogo"
-            alt=""
-          />
-        </transition>
+        > -->
+        <nuxt-img class="object-center" src="/pic/CC.jpg" alt="" />
+        <!-- </transition> -->
       </div>
       <Logo />
     </div>
